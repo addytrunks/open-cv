4acb218a5b6528845c8aa42e836fcebe85d2e4b6
Adhithya Srivatsan <addytrunks@gmail.com>
2024-05-29 09:11:10 +0530

    feat: essentials

---
+++ ESSENTIALS/essentials.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+img = cv.imread('Resources\image2.jpg')
+cv.imshow('Image', img)   
+
+# Converting to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Grayscaled', gray)
+
+# Blur
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blurred', blur)
+
+# Edge Cascade (find edges)
+# Passing a blurred image will reduce the number of edges detected
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=3)
+cv.imshow('Dilated', dilated)
+
+#Eroding
+eroded = cv.erode(dilated, (7,7), iterations=3)
+cv.imshow('Eroded', eroded)
+
+cv.waitKey(0)
