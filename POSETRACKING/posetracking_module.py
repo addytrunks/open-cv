5139d25a75de8c03c78cb9e184e25dfa334ed54e
Adhithya Srivatsan <addytrunks@gmail.com>
2024-06-13 19:53:08 +0530

    feat: add findAngle methid

--- POSETRACKING/posetracking_module.py
+++ POSETRACKING/posetracking_module.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 
 class PoseTracking:
@@ -42,3 +43,26 @@ class PoseTracking:
                 if draw:
                     cv2.circle(image, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return self.lmList
+
+    def findAngle(self, image, p1, p2, p3, draw=True):
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate the angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+
+        if angle < 0:
+            angle+=360
+
+        if draw:
+            cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 2)
+            cv2.line(image, (x3, y3), (x2, y2), (255, 255, 255), 2)
+            cv2.circle(image, (x1, y1), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(image, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(image, (x3, y3), 10, (255, 0, 0), cv2.FILLED)
+            cv2.putText(image, str(int(angle)), (x2 - 20, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0),2)
+
+        return angle
